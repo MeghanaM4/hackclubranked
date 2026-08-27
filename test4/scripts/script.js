973879e9c60a4e5b171c8c1c9e0189d0a65ad8be
Meghana Madiraju <meghanambpd@gmail.com>
2026-08-27 12:10:55 -0700

    Publish test4

---
+++ test4/scripts/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.blob')?.addEventListener('click', () => {
+  document.body.classList.toggle('clicked');
+});document.querySelector('.blob')?.addEventListener('click', () => {
+  document.body.classList.toggle('clicked');
+});
